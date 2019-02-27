d085debeac55b077fba24bf627f82c97ea2b2092
Josh Berkenmeier <joshuaberkenmeier@u.boisestate.edu>
2019-02-26 22:49:15 -0700

    added admin login, meal idea start, mysql script.. separated nav/footer

--- home.php
+++ home.php
@@ -18,26 +18,10 @@
 </head>
 
 <body>
-<nav class="navbar navbar-default navbar-fixed-top" id="nav-shadow">
-        <div class="navbar-header pull-left">
-            <a class="navbar-brand" id="navbar-brand-padding" href="http://interfaithsanctuary.org/">
-                <img class="brand" alt="Brand" id="navbar-brand-size" src="/images/Interfaith-Temp-Logo.png">
-            </a>
-            <a class="navbar-brand" id="navbar-brand-font" href="/home.php">Adopt a Meal</a>
-        </div>
-        <!-- <button type="button" class="navbar-toggle collapsed pull-right" data-toggle="collapse" data-target="#navigation">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-        </button> -->
-        <div class="collapse navbar-collapse pull-right" id="navigation">
-            <ul class="nav navbar-nav">
-                <li class="nav-item "><a class="navbar-link" href="/home.php">Calendar View</a></li>
-                <li class="nav-item "><a class="navbar-link" href="/mealIdeas.php">Meal Ideas</a></li>
-                <li class="nav-item "><a class="navbar-link" href="/adminLogin.php">Admin</a></li>
-            </ul>
-        </div>
-    </nav>
+
+<?php 
+    include('nav.php'); 
+?>
 
     <!-- <img src="images/Interfaith-Temp-Logo-Other.png" width="100" height="70"> -->
     <div class="text-center jumbotron">
@@ -75,36 +59,12 @@
         <div class="row">
                     </div>
     </div>
-    <!-- <div class="text-center jumbotron jumbotron-footer">
-        <p>We would like to thank all the organizations who have volunteered for their wonderful contributions!</p>
-        <div class="row">
-            <div class="list-group-item thank-you-list-item col-md-2 md-col-3 lg-col-4" >
-                <h3>
-                    
-                </h3>
-            </div>
-        </div>
-    </div>  -->
-    <!-- past event modal -->
-    <!-- <div id="past-event-modal" class="modal fade" role="dialog">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h3 id="title" style="margin-top: 15px;">This event is in the past</h3>
-                </div>
-                <div class="modal-body">
-                </div>
-                <div class="modal-footer"></div>
-            </div>
-        </div>
-    </div> -->
-
-    <div id="footer">
-        <footer>
-            <li id="first">© 2019 Interfaith Sanctuary</li>
-            <li>Contact Admin: <a id="adminEmail" href="mailto: ">TODO</a></li>
-        </footer>
-    </div>
+ <div class="row">   
+<?php 
+    include('footer.php'); 
+?>
+</div>
 </body>
+
 
 </html>
