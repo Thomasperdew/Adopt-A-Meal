a137dfb2e36eff20eecbc1a4c9955cfcebcaad52
Jaime David Guevara Rojas <jaimeguevara@u.boisestate.edu>
2019-03-05 09:38:30 -0700

    Updated admin login php

--- adminLogin.php
+++ adminLogin.php
@@ -7,30 +7,23 @@
     <link rel="shortcut icon" type="image/x-icon" href="./images/favicon.ico"/>
 </head>
 
-<?php 
-    include('nav.php'); 
-?>
+<body>
+    <?php include('nav.php'); ?>
+    <form method="post" action="/loginhandler.php" enctype="multipart/form-data">
+    <h2> ADMIN LOGIN </h2>
+    <div id="login">
+        <label for="username"><b>Username</b></label><br>
+        <input type="text" id="ip2" placeholder="Enter Username"  value="<?php echo isset($_SESSION['presets']['username']) ? $_SESSION['presets']['username'] : ''; ?>" name="username" required><br>
 
-<form method="post" action="/loginhandler.php" enctype="multipart/form-data">
-<h2> ADMIN LOGIN </h2>
-<div id="login">
-  <label for="username"><b>Username</b></label><br>
-  <input type="text" id="ip2" placeholder="Enter Username"  value="<?php echo isset($_SESSION['presets']['username']) ? $_SESSION['presets']['username'] : ''; ?>" name="username" required><br>
-
-  <label for="password"><b>Password</b></label><br>
-  <input type="password" id="ip2" placeholder="Enter Password"  value="<?php echo isset($_SESSION['presets']['password']) ? $_SESSION['presets']['password'] : ''; ?>" name="password" required><br>
-      
-  <button type="submit">Login</button>
-  <button type="reset" class="cancelbtn">Cancel</button>
-</div>
-
-<div id="bottom_login">
-  
-</div>
-</form>
-
-<?php 
-    include('footer.php'); 
-?>
-
+        <label for="password"><b>Password</b></label>
+        </br>
+        <input type="password" id="ip2" placeholder="Enter Password"  value="<?php echo isset($_SESSION['presets']['password']) ? $_SESSION['presets']['password'] : ''; ?>" name="password" required>
+        </br>
+        <button type="submit">Login</button>
+        <button type="reset" class="cancelbtn">Cancel</button>
+    </div>
+    <div id="bottom_login"></div>
+    </form>
+    <?php include('footer.php'); ?>
+</body>
 </html>
