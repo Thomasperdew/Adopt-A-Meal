7e389b143f5e569a8d46912bfaeead187bcee889
Jaime David Guevara Rojas <jaimeguevara@u.boisestate.edu>
2019-03-15 09:13:26 -0600

    Adding the javascript line referencing home.js as this file does not exist and is throwing the FTP server off

--- home.php
+++ home.php
@@ -11,7 +11,7 @@
 <html>
 <head>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script type="text/javascript" src="js/home.js"></script>
+    <!-- <script type="text/javascript" src="js/home.js"></script> -->
     <link rel="stylesheet" type="text/css" href="css/interfaith.css">
     <title>Adopt-A-Meal - Home</title>
     <link rel="shortcut icon" type="image/x-icon" href="./images/favicon.ico"/>
