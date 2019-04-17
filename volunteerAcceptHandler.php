fef2f7992b9454888ef5e18409271a7e599415c6
Jaime David Guevara Rojas <jaimeguevara@u.boisestate.edu>
2019-04-17 16:07:19 -0600

    Volunteer accept handler update

--- volunteerAcceptHandler.php
+++ volunteerAcceptHandler.php
@@ -2,13 +2,13 @@
 require_once 'Dao.php';
 $dao = new Dao();
 session_start();
-//$send_to =  $dao->getEmails();
-//$subject = "Volunteer Accepted";
+$send_to =  $dao->getEmails();
+$subject = "Volunteer Accepted";
 
 $id = $_POST['accVol'];
 $dao->acceptVolunteer($id);
 $date = $dao->getVolunteerDateByID($id);
-// $dao->rejectNonAcceptedVolunteers($id, $date);
+$dao->rejectNonAcceptedVolunteers($id, $date);
 $dao->removeDate($date);
 $message = "A meal idea, for the following date: " . $date . ", has been accepted by " . $_SESSION['username'] . ".";
 mail($send_to,$subject,$message);
