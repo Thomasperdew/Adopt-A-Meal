7dfa37ad59e5fd6f343f23bddf7ee6ef5d30920a
Jaime David Guevara Rojas <jaimeguevara@u.boisestate.edu>
2019-04-17 16:04:59 -0600

    Update to handlers

--- mealAcceptHandler.php
+++ mealAcceptHandler.php
@@ -8,12 +8,7 @@ $message = "A meal idea has been accepted by " . $_SESSION['username'] . ".";
 
 $id = $_POST['btn'];
 $dao->acceptMealIdea($id);
-<<<<<<< HEAD
-$_SESSION['messagePending'][]= "Meal idea has been accepted";
-mail($send_to,$subject,$message);
-=======
 $_SESSION['messageSuccess'][]= "Meal idea has been accepted";
 mail($to_email_address,$subject,$message);
->>>>>>> 81f582734a569ff43a7d87dc576a5814693b75d6
 header('Location: /adminMealIdeas.php');
 exit();
--- volunteerAcceptHandler.php
+++ volunteerAcceptHandler.php
@@ -10,18 +10,7 @@ $dao->acceptVolunteer($id);
 $date = $dao->getVolunteerDateByID($id);
 // $dao->rejectNonAcceptedVolunteers($id, $date);
 $dao->removeDate($date);
-<<<<<<< HEAD
-<<<<<<< HEAD
-$_SESSION['messagePending'][]= "Volunteer event has been accepted";
-=======
-$_SESSION['messageSuccess'][]= "Volunteer event has been accepted";
->>>>>>> 7be848032104e3de107f103e1db511003feb3b69
 $message = "A meal idea, for the following date: " . $date . ", has been accepted by " . $_SESSION['username'] . ".";
-//mail($send_to,$subject,$message);
-=======
-$_SESSION['messageSuccess'][]= "Volunteer event has been accepted";
-$message = "A meal idea, for the following date: " . $date . ", has been accepted by " . $_SESSION['username'] . ".";
-mail($to_email_address,$subject,$message);
->>>>>>> 81f582734a569ff43a7d87dc576a5814693b75d6
+mail($send_to,$subject,$message);
 header('Location: /adminVolunteer.php');
 exit();
